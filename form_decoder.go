2832a30dc0532e5a886b135fb1e861c362f66a44
aesoper <weilanzhuan@163.com>
2026-05-01 16:51:37 +0800

    feat(formcodec): 添加表单编码和解码基础功能
    - 新增 form_encoder.go，提供全局表单结构体编码器及 Encode 函数
    - 新增 form_decoder.go，提供全局表单结构体解码器及 Decode 函数
    - 实现结构体与 map[string][]string 之间的相互转换基础接口
    - 添加 Apache 2.0 版权和许可声明
    - 统一编码风格与包结构设计

---
+++ form_decoder.go
@@ -0,0 +1,23 @@
+// Copyright 2026 aesoper101
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package formcodec
+
+// formDecoder is a global Decoder.
+var formDecoder = NewDecoder()
+
+// Decode decodes a map[string][]string into a struct.
+func Decode(data map[string][]string, v any) error {
+	return formDecoder.Decode(data, v)
+}
